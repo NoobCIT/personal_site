f4e3714d4c6d35155bbd8ddfb1d2b6db96066170
Kevin <nguyenk215@yahoo.com>
2017-12-11 20:05:23 -0800

    Completes form-based workflow features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,35 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    flash[:success] = "You successfully created the article!"
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:success] = "Your article was successfully deleted!"
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash[:success] = "Your article was successfully updated!"
+    redirect_to article_path(@article)
+  end
+
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def bootstrap_class_for(flash_type)
+    case flash_type.to_sym
+      when :success
+        "alert-success"
+      when :error
+        "alert-error"
+      else
+        flash_type.to_s
+    end
+  end
 end
