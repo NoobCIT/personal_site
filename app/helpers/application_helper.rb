26db082512ef98977332724b5c6db5158c4d783d
Kevin <nguyenk215@yahoo.com>
2017-12-20 19:47:09 -0800

    Installs Rouge Gem
    Installs the rouge gem to allow syntax highlighting.  If the user
    wants to create a post about code, using markdowns, the code
    will automatically highlight to incorporate the appropriate
    language syntax convention.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,26 @@
 module ApplicationHelper
 
-  def bootstrap_class_for(flash_type)
-    case flash_type.to_sym
-      when :success
-        "alert-success"
-      when :error
-        "alert-error"
-      else
-        flash_type.to_s
+    def bootstrap_class_for(flash_type)
+      case flash_type.to_sym
+        when :success
+          "alert-success"
+        when :error
+          "alert-error"
+        else
+          flash_type.to_s
+      end
     end
-  end
 
-  def markdown(content)
-    options = { hard_wrap: true, filter_html: true, autolink: true, no_intra_emphasis: true, fenced_code_blocks: true }
-    @markdown ||=  Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
-    @markdown.render(content)
+  def markdown(text)
+    options = { hard_wrap: true,
+                filter_html: true,
+                autolink: true,
+                no_intra_emphasis: true,
+                link_attributes: { rel: 'nofollow', target: '_blank' }
+              }
+    renderer =  CustomHtml.new(options)
+
+    extensions = { fenced_code_blocks: true }
+    Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
   end
 end
